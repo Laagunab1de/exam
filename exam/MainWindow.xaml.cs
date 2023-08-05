05d51ed09c45b33dff7f7ebe508cb8e7aee70b64
andre <andreynidrih>
2023-08-06 03:13:26 +1000

    Добавьте файлы проекта.

---
+++ exam/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+     public partial class MainWindow : Window
+    {
+        private examContext _context = new examContext();
+        public List<Name> names { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            fullicollection(_context);
+            
+        }
+        public void fullicollection(examContext context)
+        {
+            _context = context;
+            names = _context.Names.ToList();
+            nnnname.ItemsSource = names;
+        }
+    }
+}
---
+++ exam/Name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exam
+{
+    public partial class Name
+    {
+        public string? Name1 { get; set; }
+        public string? Surname { get; set; }
+        public string? Login { get; set; }
+        public int Id { get; set; }
+    }
+}
